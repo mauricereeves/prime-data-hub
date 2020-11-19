608d60a225f4d6675745b5f4b2cdcd50529e2ced
Rick Hawes <richard.hawes@cms.hhs.gov>
2020-11-19 13:46:48 -0800

    Get it too work

--- prime-router/src/main/kotlin/azure/IngestFunction.kt
+++ prime-router/src/main/kotlin/azure/IngestFunction.kt
@@ -1,24 +1,24 @@
 package gov.cdc.prime.router.azure
 
-import com.azure.storage.blob.BlobContainerClient
-import com.azure.storage.blob.BlobServiceClientBuilder
-import com.azure.storage.queue.QueueClient
-import com.azure.storage.queue.QueueClientBuilder
-import com.azure.storage.queue.models.QueueStorageException
+import com.google.common.net.HttpHeaders
 import com.microsoft.azure.functions.*
 import com.microsoft.azure.functions.annotation.AuthorizationLevel
 import com.microsoft.azure.functions.annotation.FunctionName
 import com.microsoft.azure.functions.annotation.HttpTrigger
 import com.microsoft.azure.functions.annotation.StorageAccount
-import org.apache.commons.text.TextStringBuilder
-import java.util.*
-import java.util.function.Consumer
-import java.util.logging.Logger
+import gov.cdc.prime.router.ClientSource
+import gov.cdc.prime.router.CsvConverter
+import gov.cdc.prime.router.Metadata
+import gov.cdc.prime.router.Report
+import java.io.ByteArrayInputStream
+import java.util.logging.Level
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 class IngestFunction {
+    private val clientName = "client"
+    private val csvMimeType = "text/csv"
 
     /**
      * This function listens at endpoint "/api/report".
@@ -27,7 +27,7 @@ class IngestFunction {
      * Which returns the following upon success:
      *    {"filename":"lab1-test_results-17-42-31.csv","topic":"covid-19","schemaName":"pdi-covid-19.schema","action":"","blobURL":"http://azurite:10000/devstoreaccount1/ingested/lab1-test_results-17-42-31-pdi-covid-19.schema-3ddef736-55e1-4a45-ac41-f74086aaa654.csv"}
      */
-    @FunctionName("report")
+    @FunctionName("reports")
     @StorageAccount("AzureWebJobsStorage")
     fun run(
         @HttpTrigger(
@@ -35,53 +35,74 @@ class IngestFunction {
             methods = [HttpMethod.POST],
             authLevel = AuthorizationLevel.FUNCTION
         ) request: HttpRequestMessage<String?>,
-        context: ExecutionContext
+        context: ExecutionContext,
     ): HttpResponseMessage {
-        context.logger.info("report ingest function received an upload request.")
-
-        // Create an object representing the incoming CVS file, and validate presence of required metadata.
-        val file = try {
-            IngestedFile(request, context)
+        // First load metadata
+        try {
+            val baseDir = System.getenv("AzureWebJobsScriptRoot")
+            Metadata.loadAll("$baseDir/metadata")
         } catch (e: Exception) {
-            val msgs = TextStringBuilder()
-            Arrays.asList(*e.suppressed).forEach(Consumer { ex: Throwable -> msgs.appendln(ex.message) })
-            context.logger.severe(msgs.toString())
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(msgs.toString()).build()
+            context.logger.log(Level.SEVERE, e.message, e)
+            request
+                .createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Failed to load metadata")
+                .build()
         }
-        context.logger.info("Successfully read ${file.filename}. Preparing to queue it for processing.")
 
-        // Queue the object for further processing.  Return the metadata json as a receipt to the caller.
+        // Validate the incoming CSV by converting it to a report.
+        val report = try {
+            createReportFromRequest(request, context)
+        } catch (e: Exception) {
+            context.logger.log(Level.INFO, "Bad request from e.message", e)
+            return request
+                .createResponseBuilder(HttpStatus.BAD_REQUEST)
+                .body(e.message)
+                .build()
+        }
+        context.logger.info("Successfully read ${report.id}. Preparing to queue it for processing.")
+
+        // Queue the report for further processing.
         return try {
-            val queueClient = createQueueClient()
-            val blobContainerClient = createBlobClient()
-            file.queueForProcessing(queueClient, blobContainerClient, context)
-            context.logger.info("Successfully queued and stored blob for processing: ${file.blobURL}")
-            request.createResponseBuilder(HttpStatus.OK).body(file.toJson()).build()
+            ReportQueue.sendReport(ReportQueue.Name.INGESTED, report)
+            request
+                .createResponseBuilder(HttpStatus.CREATED)
+                .body(createResponseBody(report))
+                .build()
         } catch (e: Exception) {
-              request.createResponseBuilder(HttpStatus.BAD_REQUEST)
-                .body("This CVS failed to get queued for further work.\n").build()
+            context.logger.log(Level.SEVERE, e.message, e)
+            request
+                .createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Failed to send to queue for further work.")
+                .build()
         }
     }
 
-    companion object {
-        var ingestQueueName = "ingested"
-        var ingestContainerName = "ingested"
-
-        // Connect to the ingest queue.
-        @Throws(QueueStorageException::class)
-        fun createQueueClient(): QueueClient {
-            val connectStr = System.getenv("AzureWebJobsStorage")
-            return QueueClientBuilder()
-                .connectionString(connectStr)
-                .queueName(ingestQueueName)
-                .buildClient()
-        }
-
-        // Connect to the blob container.
-        fun createBlobClient(): BlobContainerClient {
-            val connectStr = System.getenv("AzureWebJobsStorage")
-            val blobServiceClient = BlobServiceClientBuilder().connectionString(connectStr).buildClient()
-            return blobServiceClient.getBlobContainerClient(ingestContainerName)
+    fun createReportFromRequest(request: HttpRequestMessage<String?>, context: ExecutionContext): Report {
+        // TODO: check for client-id in Auth token when implement auth
+        val clientName = request.headers.getValue(clientName)
+        val client = Metadata.findClient(clientName) ?: error("$clientName does not match known clients")
+        val contentType = request.headers.getValue(HttpHeaders.CONTENT_TYPE.toLowerCase())
+        val schema = Metadata.findSchema(client.schema) ?: error("missing schema for $clientName")
+        val content = request.body?.toByteArray() ?: error("missing body content")
+        return when (contentType) {
+            csvMimeType -> {
+                CsvConverter.read(
+                    schema,
+                    ByteArrayInputStream(content),
+                    ClientSource(organization = client.organization.name, client = client.name)
+                )
+            }
+            else -> error("$contentType is not supported, expected text/csv")
         }
     }
+
+    fun createResponseBody(report: Report): String {
+        return """
+        {
+          "id": "${report.id}"
+        }
+        """.trimIndent()
+    }
+
+
 }
